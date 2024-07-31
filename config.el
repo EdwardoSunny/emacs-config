4ce31be74d4c4e22887e745bd8bd4acd7156b72a
Edward Sun <edward.sun2015@gmail.com>
2024-07-31 11:49:45 -0700

    better ivy mode and icons

--- config.el
+++ config.el
@@ -127,29 +127,37 @@
 
 (use-package counsel
   :after ivy
-  :config (counsel-mode))
-(use-package ivy
-  :defer 0.1
   :diminish
+  :config (counsel-mode))
+
+(use-package ivy
   :bind
+  ;; ivy-resume resumes the last Ivy-based completion.
   (("C-c C-r" . ivy-resume)
    ("C-x B" . ivy-switch-buffer-other-window))
+  :diminish
   :custom
-  (setq ivy-count-format "(%d/%d) ")
   (setq ivy-use-virtual-buffers t)
+  (setq ivy-count-format "(%d/%d) ")
   (setq enable-recursive-minibuffers t)
   :config
   (ivy-mode))
+
+(use-package all-the-icons-ivy-rich
+  :ensure t
+  :init (all-the-icons-ivy-rich-mode 1))
+
 (use-package ivy-rich
   :after ivy
+  :ensure t
+  :init (ivy-rich-mode 1) ;; this gets us descriptions in M-x.
   :custom
   (ivy-virtual-abbreviate 'full
    ivy-rich-switch-buffer-align-virtual-buffer t
    ivy-rich-path-style 'abbrev)
   :config
   (ivy-set-display-transformer 'ivy-switch-buffer
-                               'ivy-rich-switch-buffer-transformer)
-  (ivy-rich-mode 1)) ;; this gets us descriptions in M-x.
+                               'ivy-rich-switch-buffer-transformer))
 (use-package swiper
   :after ivy
   :bind (("C-s" . swiper)

1612f0c684d70a3e8460fd63a18605cacd399ba8
Edward Sun <edward.sun2015@gmail.com>
2025-03-13 17:29:56 -0700

    added codeium copilot and company completions for codeium :smile:

--- init.el
+++ init.el
@@ -7,6 +7,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(codeium/metadata/api_key "4bd4f4d0-da16-41dd-bbe7-2ae8e900d365")
  '(ein:output-area-inlined-images t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
